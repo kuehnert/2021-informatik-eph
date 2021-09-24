338fab9869551974d38ee7135d00b0af072b1ea3
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-09-24 14:05:43 +0200

    Stunde vom 24.09.2021

---
+++ K01-Einstieg/P06Ganzzahlen.java
@@ -0,0 +1,26 @@
+public class P06Ganzzahlen {
+    public static void ganzzahlen() {
+        // Deklaration einer Variablen, die eine 
+        // Ganzzahl speichert
+        int x;
+        
+        // Initialisierung
+        x = 10;
+        
+        // Wertzuweisung / Expression
+        // <Variable> = <Ausdruck>
+        // Weise der Variablen <Variable> den Wert zu
+        // den der Ausdruck rechts ergibt
+        x = 20 + 30;
+        x = 20;
+
+        // Erhöhe x um 5
+        x = x + 5;
+
+        // Negative Zahlen gehen
+        x = -100;
+        
+        x = 1_000_000;
+        System.out.println(x);
+    }
+}
