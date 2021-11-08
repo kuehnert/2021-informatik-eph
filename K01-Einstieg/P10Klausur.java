6534eb41ffbed6125742ec73c515ffca899a300c
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-11-08 14:03:47 +0100

    Stunde vom 08.11.2021

--- K01-Einstieg/P10Klausur.java
+++ K01-Einstieg/P10Klausur.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class P10Klausur {
-    public static void aufgabe4() {
+    public static void aufgabe4a() {
         Scanner tastatur = new Scanner(System.in);
         int x = tastatur.nextInt();
 
@@ -13,4 +13,37 @@ public class P10Klausur {
 
         System.out.println("x: " + x);
     }
+
+    public static void aufgabe4c() {
+        Scanner tastatur = new Scanner(System.in);
+        int x = tastatur.nextInt();
+
+        if (x < 3) {
+            x = x + 5; // erhöhe x um 5
+        } 
+
+        if (x >= 3) {
+            x = x - 1; // verringere x um 1
+        }
+
+        System.out.println("x: " + x);
+    }
+
+    public static void thermometer() {
+        Scanner tastatur = new Scanner(System.in);
+        int temp = tastatur.nextInt();
+
+        if (temp < -10) {
+            System.out.println("Brrr, zu kalt");
+        } else if (temp < 5) {
+            System.out.println("Schlittschuh");
+        } else if (temp < 18) {
+            System.out.println("Dicker Pulli");
+        } else if (temp < 24) {
+            System.out.println("Lecker warm");
+        } else {
+            System.out.println("Freibad");
+        }
+    }
+
 }
