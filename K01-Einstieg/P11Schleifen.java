29d80bee8f04f4fb9b866a392ab2f803316ae2e3
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-11-16 09:36:55 +0100

    Stunde vom 16.11.2021

---
+++ K01-Einstieg/P11Schleifen.java
@@ -0,0 +1,57 @@
+public class P11Schleifen {
+    
+    public static void schleife1() {
+        // while (<ausdruck>) { ... Befehle ...}
+        int geld = 0;
+        while (geld < 10) { 
+            System.out.println("Franz: Bitte gib mir Geld");
+            System.out.println("Nino wirft einen Euro in den Hut");
+            geld = geld + 1;
+            System.out.println("Franz hat " + geld + " Euro");
+        }
+        
+        System.out.println("Franz kann den Big Mac kaufen");
+    }
+    
+    // Hausis
+    // 1. Schreiben Sie eine Schleife welche die 7er-Reihe von 1 bis 140 ausgibt
+    // 2. Schreiben Sie eine Schleife welche die Zahlen von 1 bis 200 ausgibt,
+    //    außer die, die durch 7 teilbar sind
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void schleife2() {
+    }
+    
+    public static void schleife3() {
+    }
+    
+    public static void schleife4() {
+    }
+    
+    public static void schleife5() {
+    }
+    
+    public static void schleife6() {
+    }
+    
+    public static void schleife7() {
+    }
+    
+    public static void schleife8() {
+    }
+}
