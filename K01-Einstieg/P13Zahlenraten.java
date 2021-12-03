c957544f89444ee07785e0c7de9f0d18f40c5f2a
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2021-12-03 13:50:07 +0100

    Stunde vom 03.12.2021

--- K01-Einstieg/P13Zahlenraten.java
+++ K01-Einstieg/P13Zahlenraten.java
@@ -14,7 +14,78 @@ public class P13Zahlenraten {
     public static void zahlenraten() {
         Random generator = new Random();
         Scanner tastatur = new Scanner(System.in);
-        
+
+        // Würfel eine Zufallszahl zwischen 1 und 100
         int computerZahl = generator.nextInt(100) + 1;
+        int benutzerZahl = -1;
+
+        System.out.println("Willkommen beim Zahlenraten");
+        System.out.println("Versuche, eine Zahl von 1 bis 100 zu raten");
+
+        while (computerZahl != benutzerZahl) {
+            System.out.print("Dein Versuch: ");
+            benutzerZahl = tastatur.nextInt();
+
+            if (computerZahl > benutzerZahl) {
+                System.out.println("Meine Zahl ist groesser!");
+            } else if (computerZahl < benutzerZahl) {
+                System.out.println("Meine Zahl ist kleiner!");
+            } 
+        }
+
+        System.out.println("Hurra, Du hast sie geraten!");
+    }
+
+    public static void zahlenraten2() {
+        Random generator = new Random();
+        Scanner tastatur = new Scanner(System.in);
+
+        // Würfel eine Zufallszahl zwischen 1 und 100
+        int computerZahl = generator.nextInt(100) + 1;
+        int benutzerZahl;
+
+        System.out.println("Willkommen beim Zahlenraten");
+        System.out.println("Versuche, eine Zahl von 1 bis 100 zu raten");
+
+        do {
+            System.out.print("Dein Versuch: ");
+            benutzerZahl = tastatur.nextInt();
+
+            if (computerZahl > benutzerZahl) {
+                System.out.println("Meine Zahl ist groesser!");
+            } else if (computerZahl < benutzerZahl) {
+                System.out.println("Meine Zahl ist kleiner!");
+            } 
+        } while (computerZahl != benutzerZahl);
+
+        System.out.println("Hurra, Du hast sie geraten!");
+    }
+
+    public static void zahlenraten3() {
+        Random generator = new Random();
+        Scanner tastatur = new Scanner(System.in);
+        int versuche = 0;
+
+        // Würfel eine Zufallszahl zwischen 1 und 100
+        int computerZahl = generator.nextInt(100) + 1;
+        int benutzerZahl;
+
+        System.out.println("Willkommen beim Zahlenraten");
+        System.out.println("Versuche, eine Zahl von 1 bis 100 zu raten");
+
+        do {
+            System.out.print("Dein Versuch: ");
+            benutzerZahl = tastatur.nextInt();
+            versuche = versuche + 1;
+
+            if (computerZahl > benutzerZahl) {
+                System.out.println("Meine Zahl ist groesser!");
+            } else if (computerZahl < benutzerZahl) {
+                System.out.println("Meine Zahl ist kleiner!");
+            } 
+        } while (computerZahl != benutzerZahl);
+
+        System.out.println("Hurra, Du hast sie geraten!");
+        System.out.println("Du hast nur "+versuche+" Versuche gebraucht.");
     }
 }
