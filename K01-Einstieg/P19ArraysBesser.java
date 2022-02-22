d7350c2f1d90d3d01a4503e113e202d1d7834664
Matthias Kühnert <matthias.kuehnert@marienschule.com>
2022-02-22 09:32:25 +0100

    Stunde vom 22.02.2022

--- K01-Einstieg/P19ArraysBesser.java
+++ K01-Einstieg/P19ArraysBesser.java
@@ -1,18 +1,52 @@
 public class P19ArraysBesser {
     // Dreierreihe
-    // Soll ein Array mit x Elemente erzeugen, die die 7-Reihe
+    // Soll ein Array mit <anzahl> Elementen erzeugen, die die 7-Reihe
     // darstellen.
     // 3 => {7, 14, 21}
     // 5 => {7, 14, 21, 28, 35}
     // Rückgabe: das fertige Array
     // Pro: Zusätzlicher Parameter, der die Art der Reihe bestimmt
- 
+    public static int[] reihen(int anzahl, int basis) {
+        int[] zahlen;
+        zahlen = new int[anzahl];
+
+        for (int i = 0; i < anzahl; i = i + 1) {
+            zahlen[i] = (i + 1) * basis;
+        }
+
+        return zahlen;
+    }
+
     // Zufallszahlen
     // Erstelle ein Array mit n Elementen, mit Werten bis zu max
     // 2 Parameter: 
     // n: Größe des Arrays
     // p: Maximale Höhe der Zahlen -1
-    
+
     // Absteigend
-    // 
+    public static int[] absteigend(int laenge) {
+        int[] zahlen;
+        zahlen = new int[laenge];
+        
+        for (int i = 0; i < zahlen.length; i = i + 1) {
+            zahlen[i] = zahlen.length - 1 - i;
+        }
+        
+        return zahlen;
+    }
+
+    public static void main() {
+        int[] rueckgabe;
+        rueckgabe = reihen(20, 18);
+        P18Arrays.ausgeben(rueckgabe);
+        System.out.println("-----------------------------------");
+        
+        rueckgabe = absteigend(7);
+        P18Arrays.ausgeben(rueckgabe);
+        
+    }
+    
+    // String eingabe = tastatur.nextLine();
+    // int zahl = Integer.parseInt(eingabe);
+    // benutzerZahlen[i] = zahl;
 }
---
+++ K01-Einstieg/P20Mathespiel.java
@@ -0,0 +1,12 @@
+public class P20Mathespiel {
+    public static void main() {
+        // Wähle zufällig eine Zahlenreihe aus, die der Benutzer
+        // vervollständigen soll.
+        
+        // Lies 10 Zahlen vom Benutzer ein
+        
+        // Danach vergleiche die Eingaben mit den korrekten Werten
+        // Und gib aus, wieviele Zahlen richtig bzw. falsch sind
+        // Und gib eine Erfolgsmeldung aus
+    }
+}
